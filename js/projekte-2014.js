905804e30df570c4d0e720d709dd8c279e138f69
Knut Perseke <knut.perseke@gmail.com>
2015-04-22 23:40:32 +0200

    started implementation of project navigation

--- js/projekte-2014.js
+++ js/projekte-2014.js
@@ -5,11 +5,55 @@ $(document).ready(function () {
 	var projects = {};
 	var persons = {};
 
+	currentProject = 0;
+
+
+	function getSymbolByLinktype(type) {
+		return "S";
+	}
+
+	function getSymbolForContactType(type) {
+		return '#';
+	}
+
+	function fillProjectInformation(currentProject) {
+		$('#projectlinks').html('');
+		$.each(currentProject.links, function(index, val) {
+			var symbol = getSymbolByLinktype(val.class)
+			$('#projectlinks').append(val.text + ' <a href="' + val.link + '">' + symbol + '</a><br />');
+		});
+
+		$('#data').html('');
+		$.each(currentProject.data, function(index, val){
+			$('#data').append(val + ",<br />");
+		});
+	}
+
+	function updateTeam(member) {
+		$('#team').html('');
+		$.each(member, function(index, val) {
+			currentMember = persons[val];
+			$('#team').append(currentMember.name + '<br />');
+		});
+	}
+
 	function setProject(index) {
 		var currentProject = projects[index];
+		console.log(currentProject);
 		$('#project-name').html(currentProject.name);
 		$('#project-description').html(currentProject.description);
 		$('#project-image').attr('src', currentProject.images[0]);
+
+		if (index < projects.length) {
+			$('#next-project').html('<a href="#?project_id=' + ++index + '">Nächstes Projekt</a>');
+		}
+
+		if (index > 0) {
+			$('#previous-project').html('<a href="#?project_id=' + --index + '">Vorheriges Projekt</a>');
+		}
+		
+		fillProjectInformation(currentProject);
+		updateTeam(currentProject.team)
 		console.log(currentProject);
 	}
 
@@ -18,4 +62,14 @@ $(document).ready(function () {
 		persons = data['persons'];
 		setProject(0);
 	});
+
+	$('#next-project').click(function() {
+		currentProject++;
+		setProject(currentProject);
+	})
+
+	$('#previous-project').click(function() {
+		currentProject--;
+		setProject(currentProject);
+	})
 });
